dd38c9a8e263f473ff558ef15027fefd598d0535
Oleksandr Bakum <sannuha84@gmail.com>
2023-06-26 20:05:24 +0300

    final stage on only css

--- book-list/main.js
+++ book-list/main.js
@@ -1,27 +1,53 @@
-const booksNumber = document.getElementById("count-books"),
-totalPrice = document.getElementById("total-price"),
-popUp = document.querySelector(".pop-up");
-let n = 1,
-amount = 42; // change later with fetch and json()
+const root = document.getElementById("root")
 
-function calculateTotalPrice() {
-    n = booksNumber.value;
-    let price = 29.99; // change later with fetch and json()
-    totalPrice.textContent = `${(n * price).toFixed(2)}`;
-}
-
-function validateInput() {
-    booksNumber.value = booksNumber.value.replace(/^0+/,'')
-    booksNumber.style.color = "black";
-    popUp.classList.remove("show");
-    if (booksNumber.value > amount) {
-        booksNumber.value = amount;           
-        booksNumber.style.color = "red";
-        popUp.classList.add("show");
+async function getBooks(url) {
+    try { 
+        let response = await fetch(url);
+        if (response.status === 200) {
+            let arrayBooks = await response.json();
+            displayBooks(arrayBooks.books);
+        }
+        else {
+            console.log("Error: " + response.status);
+        }
+    } catch (error) {
+        console.log("Error ", error);
     }
 }
 
-booksNumber.addEventListener("change", validateInput);
-booksNumber.addEventListener("change", calculateTotalPrice);
-booksNumber.addEventListener("keyup", validateInput);
-booksNumber.addEventListener("keyup", calculateTotalPrice);
+function displayBooks(books) {
+    for (const book of books) {
+        const bookItem = createBook(book);
+        root.appendChild(bookItem);
+      }
+}
+
+function createBook (book) {
+        const container = document.createElement("div");
+        container.classList.add("book-item");
+        const cover = document.createElement("img");
+        cover.classList.add("book-cover");
+        cover.src = book.image;
+        cover.alt = book.title;
+        const textContainer = document.createElement("div");
+        textContainer.classList.add("text-container")
+        const title = document.createElement("h3");
+        const author = document.createElement("p");
+        const price = document.createElement("p");
+        price.classList.add("bold-text", "price-text");
+        const button = document.createElement("button");
+        button.classList.add("main-button", "view-button");
+        title.textContent = book.title;
+        author.textContent = book.author;
+        price.textContent = book.price;
+        button.textContent = "View";
+        container.appendChild(cover);
+        textContainer.appendChild(title);
+        textContainer.appendChild(author);
+        textContainer.appendChild(price);
+        container.appendChild(textContainer);
+        container.appendChild(button);
+        return container;
+}
+
+getBooks("../books.json");
