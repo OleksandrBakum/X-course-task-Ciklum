2940a75c1dc2e22aaadbbba464e7e1736736de04
Oleksandr Bakum <sannuha84@gmail.com>
2023-07-03 21:21:06 +0300

    Finally made RWWD

--- book-list/main.js
+++ book-list/main.js
@@ -25,28 +25,36 @@ function displayBooks(books) {
 function createBook (book) {
         const container = document.createElement("div");
         container.classList.add("book-item");
+        const imageContainer = document.createElement("div");
+        imageContainer.classList.add("cover-container");
         const cover = document.createElement("img");
-        cover.classList.add("book-cover");
+        cover.classList.add("book-cover", "pointer");
         cover.src = book.image;
         cover.alt = book.title;
         const textContainer = document.createElement("div");
         textContainer.classList.add("text-container")
         const title = document.createElement("h3");
         const author = document.createElement("p");
+        
+        const priceButtonContainer = document.createElement("div");
+        priceButtonContainer.classList.add("price-button-container");
+
         const price = document.createElement("p");
         price.classList.add("bold-text", "price-text");
         const button = document.createElement("button");
-        button.classList.add("main-button", "view-button");
+        button.classList.add("main-button", "view-button", "bold-text", "rounded", "pointer");
         title.textContent = book.title;
         author.textContent = book.author;
         price.textContent = book.price;
         button.textContent = "View";
-        container.appendChild(cover);
+        container.appendChild(imageContainer);
+        imageContainer.appendChild(cover);
         textContainer.appendChild(title);
         textContainer.appendChild(author);
-        textContainer.appendChild(price);
         container.appendChild(textContainer);
-        container.appendChild(button);
+        container.appendChild(priceButtonContainer);
+        priceButtonContainer.appendChild(price);
+        priceButtonContainer.appendChild(button);
         return container;
 }
 
